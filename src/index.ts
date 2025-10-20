29fa8e69fe9a473f3c9e041ce7d58e587fe09f71
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-20 16:45:23 -0300

    montagem básica dos modulos

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,19 @@
-import makeWASocket, { useMultiFileAuthState, fetchLatestBaileysVersion } from "@whiskeysockets/baileys";
-import qrcode from "qrcode-terminal";
-import { groupCache } from "./storage/cache.js";
 
-export async function start() {
-  const { state, saveCreds } = await useMultiFileAuthState("./auth_info");
-  const { version } = await fetchLatestBaileysVersion();
+import makeWASocket, { useMultiFileAuthState } from '@whiskeysockets/baileys';
+import { WhatsAppBot } from './infra/bot/bot.js';
+import { AddAvisoUseCase } from './usecases/AddAvisoUsecase.js';
+import { ListarAvisosUseCase } from './usecases/ListarAvisoUsecase.js';
+import { createBaileysConnection } from './infra/bot/BaileysConnection.js';
+import { AvisoRepository } from './infra/database/AvisoRepository.js';
 
-  const sock = makeWASocket({
-    auth: state,
-    version: version as [number, number, number],
-    printQRInTerminal: true,
-    cachedGroupMetadata: async (jid) => groupCache.get(jid)
-  });
+async function main() {
+  const { sock, groupCache } = await createBaileysConnection();
 
-  sock.ev.on("connection.update", (update) => {
-    if (update.qr) qrcode.generate(update.qr, { small: true });
-  });
+  const avisoRepo = new AvisoRepository();
+  const addAviso = new AddAvisoUseCase(avisoRepo);
+  const listarAvisos = new ListarAvisosUseCase(avisoRepo);
+  const bot = new WhatsAppBot(addAviso, listarAvisos, sock);
 
-  sock.ev.on("creds.update", saveCreds);
-
-  sock.ev.on('groups.update', async ([event]) => {
-    const metadata = await sock.groupMetadata(event!.id!)
-    groupCache.set(event!.id!, metadata)
-})
-
-sock.ev.on('group-participants.update', async (event) => {
-    const metadata = await sock.groupMetadata(event.id)
-    groupCache.set(event.id, metadata)
-})
 }
 
-start();
+main();
