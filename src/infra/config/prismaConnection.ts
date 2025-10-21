3cd25cddd107c7e2d3f14a4bcd7402d76c351a8c
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-21 17:05:34 -0300

    adicionando log de connection

--- src/infra/config/prismaConnection.ts
+++ src/infra/config/prismaConnection.ts
@@ -6,4 +6,16 @@ export const Prisma =
   globalForPrisma.Prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.Prisma = Prisma
+
+async function testConnection() {
+  try {
+    await Prisma.$connect()
+    console.log("✅ Conectado ao MongoDB Atlas com sucesso!");
+  } catch (error: any) {
+    throw new Error("Erro ao conectar ao mongoDb", error)
+  }finally{
+    await Prisma.$disconnect()
+  }
+}
+testConnection()
     
