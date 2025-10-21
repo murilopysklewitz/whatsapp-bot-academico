b9fcc0d49900acce44ffb28d50b73c1f4e6f1aca
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-21 15:18:47 -0300

    adicionando a connection com prisma

--- src/infra/config/prismaConnection.ts
+++ src/infra/config/prismaConnection.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { Prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient()
+export const Prisma =
+  globalForPrisma.Prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.Prisma = Prisma
     
