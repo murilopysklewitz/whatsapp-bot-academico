7cab88d1a8b8fc2f77d9af2d8e14a74f17cb8a4b
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-20 23:54:19 -0300

    montando os módulos

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { PingCommand } from "./application/Commands/PingCommand.js";
+import { WhatsappBot } from "./infra/bot/WhatsappBot.js";
+import { createBaileysConnection } from "./infra/config/BaileysConnection.js";
+
+async function main() {
+    const pingCommand = new PingCommand();
+    const bot = new WhatsappBot(pingCommand);    
+
+    const { sock } = await createBaileysConnection(
+        async (chatId, message, participant) => {
+          await bot.handleMessage(chatId, message, sock);
+        }
+      );
+    console.log(' Bot pronto! Envie /ping no WhatsApp\n');
+}
+main().catch(error => {
+    console.error(' Erro fatal:', error);
+    process.exit(1);
+  });
