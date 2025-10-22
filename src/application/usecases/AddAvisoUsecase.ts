767828f74b3d7d4854b8b63695a9dca73a2f59a3
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-22 12:34:51 -0300

    fazendo a criação de um id e código válido

--- src/application/usecases/AddAvisoUsecase.ts
+++ src/application/usecases/AddAvisoUsecase.ts
@@ -1,10 +1,14 @@
+import { randomBytes } from "crypto";
+import { ObjectId } from "mongodb";
 import { Aviso } from "../../domain/entities/Aviso.js";
 import { AvisosGateway } from "../../domain/gateway/AvisosGateway.js";
 
 export class AddAvisoUsecase {
     constructor(private readonly gateway: AvisosGateway){}
     async execute(chatId: string, data: string, message: string): Promise<void> {
-        const aviso = Aviso.create(chatId, data, message)
+        const id = new ObjectId().toHexString();
+        const codigo = randomBytes(3).toString('hex').toUpperCase();
+        const aviso = Aviso.create(id, codigo, chatId, data, message)
         await this.gateway.save(aviso)
     }
 }
