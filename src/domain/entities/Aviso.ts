4701ef7ca6aff7d2551eac6e0ee7c1e99a394af2
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-22 12:34:30 -0300

    fiz com que os métodos create e fromDatabase apenas recebessem o codigo e o id não criassem

--- src/domain/entities/Aviso.ts
+++ src/domain/entities/Aviso.ts
@@ -2,18 +2,26 @@ import { randomBytes, randomUUID } from "crypto"
 
 export class Aviso {
     private constructor(
-        readonly id: string | undefined,
+        readonly id: string,
         readonly codigo: string,
         readonly chatId: string,
         readonly data: string,
         readonly message: string,
     ) {}
 
-    public static create(chatId: string, message: string, data: string) {
-        const codigo = randomBytes(3).toString('hex').toUpperCase()
-        return new Aviso(undefined, codigo, chatId, data, message)
+    public static create(id: string, codigo: string, chatId: string, message: string, data: string) {
+        return new Aviso(id, codigo, chatId, data, message)
     }
 
+    public static fromDatabase(
+        id: string,
+        codigo: string,
+        chatId: string,
+        data: string,
+        message: string
+    ) {
+        return new Aviso(id, codigo, chatId, data, message);
+    }
 
     
 }
