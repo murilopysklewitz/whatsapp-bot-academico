223ee496f9bd9d69aac7e4ddce9d3d26d57bae6e
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-23 16:54:15 -0300

    adicionando as mudanças para meu usecase não criar o ID

--- src/application/usecases/AddAvisoUsecase.ts
+++ src/application/usecases/AddAvisoUsecase.ts
@@ -1,14 +1,15 @@
 import { randomBytes } from "crypto";
-import { ObjectId } from "mongodb";
+import { ObjectId } from "bson";
 import { Aviso } from "../../domain/entities/Aviso.js";
 import { AvisosGateway } from "../../domain/gateway/AvisosGateway.js";
+import dayjs from "dayjs";
 
 export class AddAvisoUsecase {
     constructor(private readonly gateway: AvisosGateway){}
     async execute(chatId: string, data: string, message: string): Promise<void> {
-        const id = new ObjectId().toHexString();
         const codigo = randomBytes(3).toString('hex').toUpperCase();
-        const aviso = Aviso.create(id, codigo, chatId, data, message)
+
+        const aviso = Aviso.create(codigo, chatId, message, data)
         await this.gateway.save(aviso)
     }
 }
