c82ded8c103eedb837edfc5c9cf1a41b5f0078b6
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-24 12:19:08 -0300

    fazendo a validação da data

--- src/application/usecases/AddAvisoUsecase.ts
+++ src/application/usecases/AddAvisoUsecase.ts
@@ -8,8 +8,25 @@ export class AddAvisoUsecase {
     constructor(private readonly gateway: AvisosGateway){}
     async execute(chatId: string, data: string, message: string): Promise<void> {
         const codigo = randomBytes(3).toString('hex').toUpperCase();
+        if (!this.isValidData(data)) {
+            throw new Error('Data inválida. Use o formato DD/MM (ex: 25/12)');
+          }
 
         const aviso = Aviso.create(codigo, chatId, message, data)
         await this.gateway.save(aviso)
+    }
+
+    private isValidData(data:string): boolean {
+        const regex = /^(\d{1,2})\/(\d{1,2})$/;
+        if (!regex.test(data)) return false;
+
+        const [dia, mes] = data.split('/').map(Number)
+        if (!dia || !mes || dia < 1 || dia > 31 || mes < 1 || mes > 12) {
+            return false;
+          }
+
+        const ano = dayjs().year()
+        const dataCompleta = dayjs(`${ano}-${mes}-${dia}`, 'YYYY-M-D', true)
+        return dataCompleta.isValid();
     }
 }
