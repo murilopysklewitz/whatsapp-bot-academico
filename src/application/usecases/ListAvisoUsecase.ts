3ff9d2af0e36d2362e8359f34db73d936ec198df
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-24 12:18:29 -0300

    tentado um método de ordenação por data

--- src/application/usecases/ListAvisoUsecase.ts
+++ src/application/usecases/ListAvisoUsecase.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { Aviso } from "../../domain/entities/Aviso.js";
 import { AvisosGateway } from "../../domain/gateway/AvisosGateway.js";
 
@@ -5,9 +6,27 @@ export class ListAvisoUsecase {
     constructor(private readonly gateway: AvisosGateway){}
 
     async execute(chatId: string): Promise<Aviso[]> {
-        const avisos = await this.gateway.list()
-        return avisos.filter(aviso => aviso.chatId === chatId);
+        const avisos = await this.gateway.list(chatId)
+        return avisos.sort((a, b) => {
+            const dataA = this.parseData(a.data)
+            const dataB = this.parseData(b.data)
+            return dataA.diff(dataB)
+        })
     }
+    private parseData(dataStr: string): dayjs.Dayjs {
+
+        const [dia, mes] = dataStr.split('/').map(Number)
+        const ano = dayjs().year()
+        
+        let data = dayjs(`${ano}-${mes}-${dia}`, 'YYYY-M-D')
+        
+
+        if (data.isBefore(dayjs(), 'day')) {
+          data = data.add(1, 'year')
+        }
+        
+        return data;
+      }
 
     
 }
