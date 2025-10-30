88ca83c86d10a45f715908c4defb4f488f423b90
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-30 12:58:18 -0300

    adicionei os novos comandos

--- src/application/Commands/HelpCommand.ts
+++ src/application/Commands/HelpCommand.ts
@@ -2,6 +2,6 @@ import { ICommands } from "./ICommands.js";
 
 export class HelpCommand implements ICommands {
     async execute(chatId: string, args: string[]): Promise<string> {
-        return 'lista de comandos:\n/ping\n/help'
+        return 'lista de comandos:\n/ping\n/help\n/avisos\n/delete'
     }
 }
